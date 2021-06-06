7fbc1901e3c53a36d43c022fd62e9666eed3ba5a
Zeeshan Haider <xeieshan@gmail.com>
2021-06-07 01:01:51 +0500

    Validating Task Filtering and Search IsOptional and IsString Directive

--- src/tasks/dto/task-filter.dto.ts
+++ src/tasks/dto/task-filter.dto.ts
@@ -1,5 +1,12 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from './../task.model';
 export class TaskFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   search?: string;
 }
